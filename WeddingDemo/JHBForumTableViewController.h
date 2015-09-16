8442faf048ddcc11d13359e147dfeae7e0c4e6f6
neuedu <neuedu@slx.local>
2015-09-16 20:44:59 +0800

    论坛功能的更新

---
+++ WeddingDemo/JHBForumTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  JHBForumTableViewController.h
+//  WeddingDemo
+//
+//  Created by neuedu on 15/9/16.
+//  Copyright (c) 2015年 Misted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JHBForumTableViewController : UITableViewController
+
+@property (assign,nonatomic)NSInteger type;
+
+@end
